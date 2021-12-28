687953e2355ff80277d0440ef7e3b6ccf88f9849
yujiale <yu18371919563@163.com>
2021-12-28 19:20:17 +0800

    新增kafka、rabbitmq、rocketmq模块

---
+++ RabbitMq/rabbitmq-basic/src/main/java/com/yjl/rabbitmq/pubsub/WeatherBureau.java
@@ -0,0 +1,28 @@
+package com.yjl.rabbitmq.pubsub;
+
+import com.yjl.rabbitmq.utils.RabbitConstant;
+import com.yjl.rabbitmq.utils.RabbitUtils;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+
+import java.util.Scanner;
+
+/**
+ * @author yjl
+ * 发布者
+ */
+public class WeatherBureau {
+
+
+    public static void main(String[] args) throws Exception {
+        Connection connection = RabbitUtils.getConnection();
+        String input = new Scanner(System.in).next();
+        Channel channel = connection.createChannel();
+
+        //第一个参数交换机名字   其他参数和之前的一样
+        channel.basicPublish(RabbitConstant.EXCHANGE_WEATHER,"" , null , input.getBytes());
+
+        channel.close();
+        connection.close();
+    }
+}
