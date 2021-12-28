b45b2538515c10b8e8f2413a7ae7c942a85fe51a
yujiale <yu18371919563@163.com>
2021-12-28 23:37:05 +0800

    rabbit中topic基本使用

--- RabbitMq/rabbitmq-basic/src/main/java/com/yjl/rabbitmq/topic/Sina.java
+++ RabbitMq/rabbitmq-basic/src/main/java/com/yjl/rabbitmq/topic/Sina.java
@@ -20,15 +20,15 @@ public class Sina {
         //声明队列信息
         channel.queueDeclare(RabbitConstant.QUEUE_SINA, false, false, false, null);
 
-        //指定队列与交换机以及routing key之间的关系
+        //指定队列与交换机以及routing key之间的关系（绑定key，bindingKey
         channel.queueBind(RabbitConstant.QUEUE_SINA, RabbitConstant.EXCHANGE_WEATHER_TOPIC, "us.#");
 
         channel.basicQos(1);
-        channel.basicConsume(RabbitConstant.QUEUE_SINA , false , new DefaultConsumer(channel){
+        channel.basicConsume(RabbitConstant.QUEUE_SINA, false, new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 System.out.println("新浪天气收到气象信息：" + new String(body));
-                channel.basicAck(envelope.getDeliveryTag() , false);
+                channel.basicAck(envelope.getDeliveryTag(), false);
             }
         });
     }
