faeab970f3b7830b75bfc8f5183e98c280446b3a
yujiale <yu18371919563@163.com>
2021-12-30 09:12:38 +0800

    rabbit高级特性之消息可靠性之消费端确认机制

--- RabbitMq/rabbitmq-advanced/src/main/java/com/yjl/rabbit/consumerAck/Consumer.java
+++ RabbitMq/rabbitmq-advanced/src/main/java/com/yjl/rabbit/consumerAck/Consumer.java
@@ -9,6 +9,8 @@ import java.io.IOException;
  * @program: springboot-example
  * @author: yjl
  * @created: 2021/12/29
+ *
+ * 消息可靠性之消费端确认机制
  */
 public class Consumer {
     public static void main(String[] args) throws IOException {
---
+++ RabbitMq/rabbitmq-advanced/src/main/java/com/yjl/rabbit/consumerQos/Consumer.java
@@ -0,0 +1,6 @@
+package com.yjl.rabbit.consumerQos;/**
+*@program: springboot-example
+*@author: yjl
+*@created: 2021/12/30
+*/public class Consumer {
+}
