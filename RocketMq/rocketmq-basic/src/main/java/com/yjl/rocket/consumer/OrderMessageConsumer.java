a1ed9b9899b1cf69d0ae267023bb42311ac6902c
yujiale <yu18371919563@163.com>
2022-01-02 12:43:58 +0800

    rocket顺序消息

--- RocketMq/rocketmq-basic/src/main/java/com/yjl/rocket/consumer/OrderMessageConsumer.java
+++ RocketMq/rocketmq-basic/src/main/java/com/yjl/rocket/consumer/OrderMessageConsumer.java
@@ -1,6 +1,52 @@
-package com.yjl.rocket.consumer;/**
-*@program: springboot-example
-*@author: yjl
-*@created: 2022/01/02
-*/public class OrderMessageConsumer {
+package com.yjl.rocket.consumer;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerOrderly;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * @program: springboot-example
+ * @author: yjl
+ * @created: 2022/01/02
+ */
+public class OrderMessageConsumer {
+
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name_3");
+        consumer.setNamesrvAddr("192.168.232.128:9876");
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+
+        consumer.subscribe("OrderTopicTest", "*");
+
+        consumer.registerMessageListener(new MessageListenerOrderly() {
+            @Override
+            public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msgs, ConsumeOrderlyContext context) {
+                context.setAutoCommit(true);
+                for(MessageExt msg:msgs){
+                    System.out.println("收到消息内容 "+new String(msg.getBody()));
+                }
+                return ConsumeOrderlyStatus.SUCCESS;
+            }
+        });
+
+//        这样是保证不了最终消费顺序的。
+//        consumer.registerMessageListener(new MessageListenerConcurrently() {
+//            @Override
+//            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+//                for(MessageExt msg:msgs){
+//                    System.out.println("收到消息内容 "+new String(msg.getBody()));
+//                }
+//                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+//            }
+//        });
+
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
+    }
 }
--- RocketMq/rocketmq-basic/src/main/java/com/yjl/rocket/producer/OrderMessageProducer.java
+++ RocketMq/rocketmq-basic/src/main/java/com/yjl/rocket/producer/OrderMessageProducer.java
@@ -1,6 +1,60 @@
-package com.yjl.rocket.producer;/**
-*@program: springboot-example
-*@author: yjl
-*@created: 2022/01/02
-*/public class OrderMessageProducer {
+package com.yjl.rocket.producer;
+
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+/**
+ * @program: springboot-example
+ * @author: yjl
+ * @created: 2022/01/02
+ */
+public class OrderMessageProducer {
+
+    public static void main(String[] args) {
+
+
+        try {
+            DefaultMQProducer defaultMQProducer = new DefaultMQProducer("");
+            defaultMQProducer.start();
+            for (int i = 0; i < 10; i++) {
+                int orderId = i;
+                for (int j = 0; j < 5; j++) {
+                    Message msg = new Message("OrderTopicTest", "order_" + orderId, "KEY" + orderId,
+                            ("order_" + orderId + " step " + j).getBytes(RemotingHelper.DEFAULT_CHARSET));
+                    SendResult sendResult = defaultMQProducer.send(msg, new MessageQueueSelector() {
+                        @Override
+                        public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                            Integer id = (Integer) arg;
+                            int index = id % mqs.size();
+                            return mqs.get(index);
+                        }
+                    }, orderId);
+
+                    System.out.printf("%s%n", sendResult);
+                }
+            }
+        } catch (MQClientException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (RemotingException e) {
+            e.printStackTrace();
+        } catch (MQBrokerException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
