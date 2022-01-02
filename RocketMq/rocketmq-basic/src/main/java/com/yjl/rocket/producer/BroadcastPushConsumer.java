b7c0154645161330b9fc6a54b007adf485b1c442
yujiale <yu18371919563@163.com>
2022-01-02 12:44:12 +0800

    rocket广播消息

--- RocketMq/rocketmq-basic/src/main/java/com/yjl/rocket/producer/BroadcastPushConsumer.java
+++ RocketMq/rocketmq-basic/src/main/java/com/yjl/rocket/producer/BroadcastPushConsumer.java
@@ -1,6 +1,43 @@
-package com.yjl.rocket.producer;/**
-*@program: springboot-example
-*@author: yjl
-*@created: 2022/01/02
-*/public class BroadcastPushConsumer {
+package com.yjl.rocket.producer;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+
+import java.util.List;
+
+/**
+ * @program: springboot-example
+ * @author: yjl
+ * @created: 2022/01/02
+ */
+public class BroadcastPushConsumer {
+
+    public static void main(String[] args) throws InterruptedException, MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name_1");
+
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+
+        consumer.setMessageModel(MessageModel.BROADCASTING);
+
+        consumer.subscribe("TopicTest", "*");
+
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        consumer.start();
+        System.out.printf("Broadcast Consumer Started.%n");
+    }
 }
