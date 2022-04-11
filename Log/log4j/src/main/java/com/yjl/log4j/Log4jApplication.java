14ec6a524cea067112f18306875bd172db9c5c71
yujiale <yjl@2020>
2022-04-11 13:58:38 +0800

    1、SpringBoot对于log4j的基本使用

---
+++ Log/log4j/src/main/java/com/yjl/log4j/Log4jApplication.java
@@ -0,0 +1,16 @@
+package com.yjl.log4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yujiale
+ */
+@SpringBootApplication
+public class Log4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Log4jApplication.class, args);
+    }
+
+}
