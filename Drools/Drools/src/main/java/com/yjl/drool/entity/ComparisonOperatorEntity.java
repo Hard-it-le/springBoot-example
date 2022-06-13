3c013d9c63398ef1cb8ca860e0e89680e5dcdae9
yujiale <yujiale@authing.cn>
2022-06-13 13:47:02 +0800

    fix：Springboot integrates the advanced use of drools

---
+++ Drools/Drools/src/main/java/com/yjl/drool/entity/ComparisonOperatorEntity.java
@@ -0,0 +1,28 @@
+package com.yjl.drool.entity;
+
+import java.util.List;
+
+/**
+ * 用于测试比较操作符
+ * @author yujiale
+ */
+public class ComparisonOperatorEntity {
+    private String names;
+    private List<String> list;
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+}
---
+++ Drools/Drools/src/main/java/com/yjl/drool/service/UserService.java
@@ -0,0 +1,10 @@
+package com.yjl.drool.service;
+
+/**
+ * @author yujiale
+ */
+public class UserService {
+    public void save(){
+        System.out.println("UserService.save()...");
+    }
+}
